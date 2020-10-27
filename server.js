f8b43eee61ee3936462a0919917f0e739979cd5e
irwinarruda <arruda.irwin@outlook.com>
2020-10-26 21:26:13 -0300

    Ajustando os Passwords comn Bcryptjs

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const routes = require("./src/routes");
+const path = require("path");
 
 // Permite requerer respostas .json
 app.use(express.json());
+app.use(express.static(path.join(__dirname + "/public")));
 
 // Configurando o Banco de dados
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://@sistemawebelo.68fnn.mongodb.net/sistemaweb", { 
+mongoose.connect("mongodb+srv://irwin:irwin@sistemawebelo.68fnn.mongodb.net/sistemaweb", { 
     useNewUrlParser: true, 
     useUnifiedTopology: true
 }).then(() => console.log("BANCO DE DADOS ACESSADO COM SUCESSO"))
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const routes = express.Router();
 const UserController = require("./controllers/UserController");
 
 routes.get("/user", UserController.index);
-routes.post("/user/new", UserController.createUser);
-routes.post("/user/check", UserController.getUser);
+routes.post("/user/register", UserController.createUser);
+routes.post("/user/login", UserController.getUser);
 
 module.exports = routes;
