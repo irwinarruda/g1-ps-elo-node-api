282fc5bf1f785f746ae2101c5114b790435fb567
irwinarruda <arruda.irwin@outlook.com>
2020-10-29 13:51:16 -0300

    Adicionando imagens

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const routes = express.Router();
 const UserController = require("./controllers/UserController");
+const multer = require("multer");
+const multerConfig = require("./config/multer");
+const upload = multer(multerConfig);
 
 routes.get("/user", UserController.index);
-routes.post("/user/register", UserController.createUser);
+routes.post("/user/register", upload.single("userImage"), UserController.createUser);
 routes.post("/user/login", UserController.getUser);
 
 module.exports = routes;
