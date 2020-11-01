86ba46c66ef817649302d7b258d4b6204e2b1a13
irwinarruda <arruda.irwin@outlook.com>
2020-11-01 14:44:38 -0300

    configuração do jwt

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -8,15 +8,6 @@ const User = mongoose.model("user");
 const imgURL = "http://localhost:3005/uploads/";
 
 module.exports = {
-    async findUsers(req, res) {
-        try {
-            const user = await User.find();
-            console.log("USUARIOS PROCURADOS COM SUCESSO");
-            return res.json(user);
-        } catch (err) {
-            console.error("ERRO AO PROCURAR USUÁRIOS: " + err);
-        } 
-    },
     async createUser(req, res) {
         try {
             const haveUser = await User.findOne({ email: req.body.email });
@@ -38,11 +29,19 @@ module.exports = {
             };
             const user = await User.create(newUser);
             console.log("USUARIO CRIADO COM SUCESSO");
+
             return res.json(user);
         } catch(err) {
-            if(req.file) fs.unlinkSync(`${req.file}`);
+            if(req.file) {
+                fs.unlinkSync(`public/uploads/${req.file.filename}`);
+            }
             console.error("ERRO AO CRIAR USUÁRIO: " + err);
-            return res.status(400).send("ERRO AO CRIAR USUÁRIO:" + err);
+            const statusNumber = 400;
+            const satusmessage = {
+                status: statusNumber,
+                error: "ERRO AO CRIAR USUÁRIO: " + err
+            }
+            return res.status(statusNumber).json(satusmessage);
         } 
     },
     async getUser(req, res) {
@@ -53,36 +52,67 @@ module.exports = {
             }
             const samePassword = await bcrypt.compare(req.body.password, user.password);
             if(samePassword){
-                const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-                console.log(token);
-            }
-            if(samePassword) {
+                const userInfo = {
+                    username: user.username,
+                    email: user.email
+                };
+                const accesstoken = jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '60s'});
+                console.log(accesstoken);
                 console.log("USUARIO LOGADO COM SUCESSO");
-                return res.json(user);
+                const accesstokenResponse = {
+                    username: user.username,
+                    email: user.email,
+                    accesstoken: accesstoken
+                }
+                return res.json(accesstokenResponse);
             } else {
                 throw new Error("SENHA INVÁLIDA");
             }
         } catch(err) {
             console.error("ERRO AO SOLICITAR USUÁRIO: " + err);
-            return res.status(400).send("ERRO AO SOLICITAR USUÁRIO: " + err);
+            const statusNumber = 400;
+            const satusmessage = {
+                status: statusNumber,
+                error: "ERRO AO SOLICITAR USUÁRIO: " + err
+            }
+            return res.status(statusNumber).json(satusmessage);
         }
     },
     async updateUser(req, res) {
         try {
+            const authenticatedUser = req.user;
             if(req.file) {
                 var pathImg = `${imgURL}${req.file.filename}`;
             } else {
                 throw new Error("IMAGEM NÃO INSERIDA");
             }
-            const user = await User.findOneAndUpdate({email: req.body.email}, {urlImg: pathImg}, {new: true});
+            const user = await User.findOne({email: authenticatedUser.email});
+            fs.unlinkSync(`public/uploads/${user.urlImg.split("/")[4]}`);
+
+            const userUpdate = await User.findOneAndUpdate({email: req.user.email}, {urlImg: pathImg}, {new: true});
             console.log("USUARIO ATUALIZADO COM SUCESSO");
-            return res.json(user);
+            return res.json(userUpdate);
         } catch(err) {
+            fs.unlinkSync(`public/uploads/${req.file.filename}`);
             console.error("ERRO AO ATUALIZAR USUÁRIO: " + err);
-            return res.status(400).send("ERRO AO ATUALIZAR USUÁRIO: " + err);
+            const statusNumber = 400;
+            const satusmessage = {
+                status: statusNumber,
+                error: "ERRO AO ATUALIZAR USUÁRIO: " + err
+            }
+            return res.status(statusNumber).json(satusmessage);
         }
     },
-    async destroy(req, res) {
+    /* async findUsers(req, res) {
+        try {
+            const user = await User.find();
+            console.log("USUARIOS PROCURADOS COM SUCESSO");
+            return res.json(user);
+        } catch (err) {
+            console.error("ERRO AO PROCURAR USUÁRIOS: " + err);
+        } 
+    }, */
+    /* async destroy(req, res) {
         try {
             User.findOneAndRemove({email: req.body.email});
             console.log("USUÁRIO REMOVIDO COM SUCESSO");
@@ -91,5 +121,5 @@ module.exports = {
             console.error("ERRO AO REMOVER USUÁRIO: " + err);
             return res.status(400).send("ERRO AO REMOVER USUÁRIO: " + err);
         }
-    }
+    } */
 }
